2b1c362b06dff759d258742331bea5eacb6956b9
OlgaKruglik <137288699+OlgaKruglik@users.noreply.github.com>
2025-01-27 20:50:05 +0300

    fix: port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import App from './App';
 import './App.css';
 
-axios.defaults.baseURL = 'http://localhost:3033'; 
+axios.defaults.baseURL = 'https://2e2e-81-199-26-3.ngrok-free.app'; 
 axios.defaults.withCredentials = true;
 
 const root = createRoot(document.getElementById('root'));
